bd7b325d083f5126f910c656ecab392be40cd159
Arash Taheri <arash.taheri@outlook.com>
2025-11-04 22:55:14 -0500

    chore: Change port

--- run.py
+++ run.py
@@ -2,10 +2,14 @@
 """
 Flask application entry point
 """
+import os
+
 from app import create_app
+
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
 
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 10000)
+    app.run(debug=True, host='0.0.0.0', port=port)
